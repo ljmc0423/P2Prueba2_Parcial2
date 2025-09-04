a2539dd2ff0bfeb4323c87b631d0148761b10d48
ljmc <ljmc2304@gmail.com>
2025-09-04 11:25:46 -0600

    primer commit

---
+++ src/prueba2parcial2/Prueba2Parcial2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba2parcial2;
+
+/**
+ *
+ * @author ljmc2
+ */
+public class Prueba2Parcial2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
